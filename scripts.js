da332fac91cb583c5cd46e412f4a65ab0dc6a796
Rafey Rana <24100103@lums.edu.pk>
2025-01-10 14:34:43 -0500

    mobile responsive

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var typed = new Typed('.typing', {
-        strings: ["Software Engineer", "Open Source Contributor", "Athlete", ""],
+        strings: ["Software Engineer", "Open Source Contributor", "Athlete", "Chess Geek"],
         loop: true,
         typeSpeed: 20,
         backSpeed: 10,
