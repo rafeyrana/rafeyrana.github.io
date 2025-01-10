41183043e58112b625bf14fa2a719e32a67564c1
Rafey Rana <24100103@lums.edu.pk>
2025-01-10 12:05:21 -0500

    index page

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed('.typing', {
+        strings: ["Software Engineer", "Open Source Contributor", "Athlete", ""],
+        loop: true,
+        typeSpeed: 20,
+        backSpeed: 10,
+        cursorChar: '▎'
+    });
+}); 
